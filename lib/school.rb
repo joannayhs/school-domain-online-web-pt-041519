1722468c6f86b172ceb9cbe9494db2b0be1bc0e2
Joanna Yuhas <joannayuhas@gmail.com>
2019-05-01 00:03:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,11 +9,14 @@ class School
     def roster
       roster = {}
     end 
+    
   end 
+    
     
   def add_student(name, grade)
     roster[grade] = []
     roster[grade] << name 
   end 
+  
     
 end 
